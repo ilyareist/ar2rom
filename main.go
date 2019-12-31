d829ad1015e5b8c8f58b76b96cd4dcd5c22dc0dc
Ilia Feoktistov <reistlin87@yandex.ru>
2019-12-31 20:13:11 +0200

    added makefile

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
-package ar2rom
+package main
+
+import (
+	"Roman-numerals/roman"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		help()
+		os.Exit(0)
+	}
+
+	var arg = os.Args[1]
+	if i, err := strconv.Atoi(arg); err != nil {
+		help()
+	} else {
+
+		var res = roman.Number{Num:i}
+		fmt.Println(res.ToRoman())
+	}
+
+}
+
+func help() {
+	fmt.Println("Usage: Converts a single integer.\ne.g.: \"./a2r 10\"")
+}
